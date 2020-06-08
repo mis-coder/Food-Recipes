623ce272732942854023ad84468605afc408beb9
Antra-Verma <51118723+Antra-Verma@users.noreply.github.com>
2020-06-08 22:21:54 +0530

    initial commit

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ const resultHeading = document.getElementById('result-heading');
 const searchAlert = document.getElementById('alert');
 const searchBtn = document.querySelector('.search-btn');
 
-
+let headingData;
+let mealsData;
 
 //---------search meal and fetch from api---------//
 function searchMeal(e){
    e.preventDefault();
 
+   document.querySelector('.header').style.background ='var(--blue) url("./images/five-white-plates-with-different-kinds-of-dishes-54455.jpg") center no-repeat';
+   document.body.style.background = 'var(--grey)';
    singleMeal.innerHTML = '';  //clear single meal
    const searchTerm = search.value; //get search term
 
@@ -43,18 +46,33 @@ function searchMeal(e){
          search.value = '';   // clear search bar
    }
    else{
+      resultHeading.innnerHTML = '';
+      meals.innerHTML = '';
       searchAlert.style.display='block';
 
-      setTimeout(() =>{
-         searchAlert.style.display='none';
-      },1000);
+      setTimeout(() => {
+      searchAlert.style.display='none';
+      resultHeading.innerHTML = '';
+   document.querySelector('.header').style.background ='none';
+      document.body.style.background ='var(--grey) url("./images/five-white-plates-with-different-kinds-of-dishes-54455.jpg") center no-repeat';
+      document.body.style.backgroundSize = 'cover';
+      meals.innerHTML = `<div class="welcome-content">
+                        <h1>Welcome to Food Recipes</h1>
+                        <p>Search for any recipes. Enjoy your meals!</p>          
+      </div>`;
+      }, 2000);
    }
 }
 
 //----to show the single meal ------//
 function addMealtoDOM(meal){
+   document.querySelector('.header').style.background ='var(--blue) url("./images/five-white-plates-with-different-kinds-of-dishes-54455.jpg") no-repeat center';
+   document.body.style.background = 'var(--grey)';
+   headingData = resultHeading.innerHTML;
+   mealsData = meals.innerHTML;
    resultHeading.innerHTML = '';
    meals.innerHTML = '';
+   document.body.scrollTop = 0;
   const ingredients = [];
 
   for(let i = 0 ; i <= 20 ;i++){
@@ -70,17 +88,18 @@ function addMealtoDOM(meal){
                          <div class="single-meal">
                              <h2>${meal.strMeal}</h2>
                              <img src=${meal.strMealThumb} alt=${meal.strMeal} />
-                             <div class="single-meal-info">
-                             ${meal.strCategory ? `<p>Meal Type: ${meal.strCategory}</p>` : ''}
-                             ${meal.strArea ? `<p>Area: ${meal.strArea}</p>` : ''}
-                             </div>
                              <div class="main">
-                             <p>${meal.strInstructions}</p>
                              <h3>Ingredients</h3>
                              <ul>
                              ${ingredients.map(ing =>`<li>${ing}</li>`).join('')}
                              </ul>
+                             <div class="single-meal-info">
+                             ${meal.strCategory ? `<p>Meal Type: ${meal.strCategory}</p>` : ''}
+                             ${meal.strArea ? `<p>Area: ${meal.strArea}</p>` : ''}
                              </div>
+                             <p class="inst"><span class="instructions">Instructions</span><br>${meal.strInstructions}</p>
+                             </div>
+                             <i class="fas fa-share-square" onclick="goBack()"></i>
                            </div>
                            
   `;
@@ -89,6 +108,8 @@ function addMealtoDOM(meal){
 //----to show the random meal----//
 function getRandomMeal(){
     //clear meal and heading
+    document.querySelector('.header').style.background ='var(--blue) url("./images/five-white-plates-with-different-kinds-of-dishes-54455.jpg") center no-repeat';
+    document.body.style.background = 'var(--grey)';
     meals.innerHTML = '';
     resultHeading.innerHTML = '';
 
@@ -96,8 +117,40 @@ function getRandomMeal(){
      .then(res => res.json())
        .then(data => {
              const meal = data.meals[0];
-
-             addMealtoDOM(meal);
+             headingData = resultHeading.innerHTML;
+             mealsData = meals.innerHTML;
+             resultHeading.innerHTML = '';
+             meals.innerHTML = '';
+             document.body.scrollTop = 0;
+            const ingredients = [];
+          
+            for(let i = 0 ; i <= 20 ;i++){
+               if(meal[`strIngredient${i}`]){
+                  ingredients.push(`${meal[`strIngredient${i}`]} : ${meal[`strMeasure${i}`]}`);
+               }
+               else{
+                  continue;
+               }
+            }
+          
+            singleMeal.innerHTML = `
+                                   <div class="single-meal">
+                                       <h2>${meal.strMeal}</h2>
+                                       <img src=${meal.strMealThumb} alt=${meal.strMeal} />
+                                       <div class="main">
+                                       <h3>Ingredients</h3>
+                                       <ul>
+                                       ${ingredients.map(ing =>`<li>${ing}</li>`).join('')}
+                                       </ul>
+                                       <div class="single-meal-info">
+                                       ${meal.strCategory ? `<p>Meal Type: ${meal.strCategory}</p>` : ''}
+                                       ${meal.strArea ? `<p>Area: ${meal.strArea}</p>` : ''}
+                                       </div>
+                                       <p class="inst"><span class="instructions">Instructions</span><br>${meal.strInstructions}</p>
+                                       </div>
+                                     </div>
+                                     
+            `;
        });
    }
 
@@ -109,6 +162,13 @@ function getMealByID(mealid){
            const theMeal = data.meals[0];
             addMealtoDOM(theMeal);
         });
+}
+
+
+function goBack(){
+     singleMeal.innerHTML = '';
+     resultHeading.innerHTML = headingData;
+     meals.innerHTML = mealsData;
 }
 
 /*---------Event Listeners--------*/
@@ -131,18 +191,14 @@ meals.addEventListener('click',e =>{
 });
 
 window.addEventListener('load',() =>{
-   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=Chocolate`)
-    .then(response => response.json())
-      .then(data =>{
-         resultHeading.innerHTML =`<h3>Search results for 'Chocolate':</h3>`;
-         meals.innerHTML = data.meals.map(meal =>
-            `<div class="results-grid-item">
-                <img src="${meal.strMealThumb}"  alt="${meal.strMeal}" />
-             <div class="meal-info" data-mealID = "${meal.idMeal}">
-             <h3>${meal.strMeal}</h3>
-             </div> 
-            </div>`
-               )
-             .join('');
-      });
+    resultHeading.innerHTML = '';
+    meals.innerHTML = `<div class="welcome-content">
+                      <h1>Welcome to Food Recipes</h1>
+                      <p>Search for any recipes. Enjoy your meals!</p>          
+    </div>`;
+});
+
+
+document.querySelector('.brand-name').addEventListener('click', ()=>{
+    location.reload();
 });
